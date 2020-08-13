7d9b42e2acca156bc8110a3773c77919f8c2fb58
Buchi Reddy B <buchi@traceable.ai>
2020-08-13 16:52:32 -0700

    More unit tests.

--- platform-metrics/src/test/java/org/hypertrace/core/serviceframework/metrics/PlatformMetricsRegistryTest.java
+++ platform-metrics/src/test/java/org/hypertrace/core/serviceframework/metrics/PlatformMetricsRegistryTest.java
@@ -7,27 +7,45 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests for {@link PlatformMetricsRegistry}
  */
 public class PlatformMetricsRegistryTest {
-  @BeforeAll
-  static void initialize() {
+  private static void initializeCustomRegistry(List<String> reporters) {
     PlatformMetricsRegistry.initMetricsRegistry("test-service",
         ConfigFactory.parseMap(Map.of(
-            "reporter.names", List.of("testing"),
+            "reporter.names", reporters,
             "reporter.prefix", "test-service",
             "reportInterval", "10",
             "defaultTags", List.of("test.name", "PlatformMetricsRegistryTest")
         )));
   }
 
+  @AfterEach
+  public void stopRegistry() {
+    PlatformMetricsRegistry.stop();
+  }
+
+  @Test
+  public void testMetricRegistryInitialization() {
+    // Make sure logging reporter initialization doesn't fail.
+    initializeCustomRegistry(List.of("logging"));
+
+    Timer timer = PlatformMetricsRegistry.registerTimer("my.timer", Map.of("foo", "bar"));
+    timer.record(1, TimeUnit.SECONDS);
+
+    // Try calling init again but that should be a NoOp.
+    initializeCustomRegistry(List.of("logging"));
+  }
+
   @Test
   public void testTimer() {
+    initializeCustomRegistry(List.of("testing"));
+
     Timer timer = PlatformMetricsRegistry.registerTimer("my.timer", Map.of("foo", "bar"));
     timer.record(1, TimeUnit.SECONDS);
     Assertions.assertEquals(1, timer.count());
@@ -48,6 +66,8 @@ public class PlatformMetricsRegistryTest {
 
   @Test
   public void testCounter() {
+    PlatformMetricsRegistry.initMetricsRegistry("test-service", ConfigFactory.empty());
+
     Counter counter = PlatformMetricsRegistry.registerCounter("my.counter", Map.of("foo", "bar"));
     counter.increment();
     Assertions.assertEquals(1, counter.count());
@@ -63,6 +83,8 @@ public class PlatformMetricsRegistryTest {
 
   @Test
   public void testGauge() {
+    initializeCustomRegistry(List.of("testing"));
+
     AtomicInteger atomicInteger = new AtomicInteger(1);
     AtomicInteger gauge = PlatformMetricsRegistry.registerGauge("my.gauge",
         Map.of("foo", "bar"), atomicInteger);
