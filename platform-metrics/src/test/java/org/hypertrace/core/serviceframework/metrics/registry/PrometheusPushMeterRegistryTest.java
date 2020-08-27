086762a066c4f980d4c5f9f43d66b7ed518a2346
inespoernomo <ines@traceable.ai>
2020-08-26 23:58:13 -0700

    Adds verifyWithRetries to ensure test robustness

--- platform-metrics/src/test/java/org/hypertrace/core/serviceframework/metrics/registry/PrometheusPushMeterRegistryTest.java
+++ platform-metrics/src/test/java/org/hypertrace/core/serviceframework/metrics/registry/PrometheusPushMeterRegistryTest.java
@@ -1,6 +1,7 @@
 package org.hypertrace.core.serviceframework.metrics.registry;
 
 import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
@@ -26,8 +27,13 @@ public class PrometheusPushMeterRegistryTest {
   public void test_init_metric_pushed() throws InterruptedException, IOException {
     PushGateway mockPushGateway = Mockito.mock(PushGateway.class);
     pushMeterRegistry = createDefault(mockPushGateway);
-    sleep(500);
-    verify(mockPushGateway, atLeastOnce()).pushAdd(any(CollectorRegistry.class), eq(JOB_NAME));
+    verifyWithRetry(200, 200, 3, () -> {
+      try {
+        verify(mockPushGateway, atLeastOnce()).pushAdd(any(CollectorRegistry.class), eq(JOB_NAME));
+      } catch (IOException e) {
+        fail("IOException is thrown when verifying metric push");
+      }
+    });
     pushMeterRegistry.stop();
   }
 
@@ -62,5 +68,26 @@ public class PrometheusPushMeterRegistryTest {
         return Duration.ofMillis(pushIntervalInMillis);
       }
      }, Executors.defaultThreadFactory(), mockPushGateway);
+  }
+
+  private void verifyWithRetry(long initialDelayInMillis, long intervalInMillis, int maxRetries, Runnable runnable)
+      throws InterruptedException {
+    int runCount = 0;
+    sleep(initialDelayInMillis);
+    while (runCount <= maxRetries){
+      try {
+        runnable.run();
+        /* break immediately if it's successful */
+        break;
+      } catch (AssertionError assertionError) {
+        /* reached max retry */
+        if (runCount == maxRetries) {
+          throw new AssertionError("Failed verification after retries");
+        }
+        sleep(intervalInMillis);
+      }
+      runCount++;
+    }
+
   }
 }
