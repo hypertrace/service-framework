17c3d154790514e2b53528f9ad51891f47df15ca
Aaron Steinfeld <aaron@traceable.ai>
2022-07-27 01:33:31 -0400

    refactor: add functional interface

--- platform-http-service-framework/src/main/java/org/hypertrace/core/serviceframework/http/HttpHandlerFactory.java
+++ platform-http-service-framework/src/main/java/org/hypertrace/core/serviceframework/http/HttpHandlerFactory.java
@@ -2,6 +2,7 @@ package org.hypertrace.core.serviceframework.http;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface HttpHandlerFactory {
   List<HttpHandlerDefinition> buildHandlers(HttpContainerEnvironment containerEnvironment);
 }
