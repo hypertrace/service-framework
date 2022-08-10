55de0086e006b6de75e81b4d058304637685a99c
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-08-09 23:44:56 -0700

    refactor: add init param config (#53)

--- platform-http-service-framework/src/main/java/org/hypertrace/core/serviceframework/http/HttpHandlerDefinition.java
+++ platform-http-service-framework/src/main/java/org/hypertrace/core/serviceframework/http/HttpHandlerDefinition.java
@@ -2,9 +2,11 @@ package org.hypertrace.core.serviceframework.http;
 
 import com.google.inject.Injector;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.Servlet;
 import lombok.Builder;
+import lombok.Singular;
 import lombok.Value;
 import lombok.experimental.Accessors;
 
@@ -19,6 +21,7 @@ public class HttpHandlerDefinition {
   CorsConfig corsConfig;
   Injector injector;
   MultipartConfigElement multipartConfig;
+  @Singular Map<String, String> servletInitParameters;
 
   @Accessors(fluent = true)
   boolean useSessions;
