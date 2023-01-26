b53095693c114628b0b2e93eaf9a1b99060d10ad
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2023-01-26 11:14:41 -0800

    chore: update dependency versions to clear up vulnerabilities (#65)

--- platform-http-service-framework/build.gradle.kts
+++ platform-http-service-framework/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
   api(project(":platform-service-framework"))
-  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.10.0")
+  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.11.2")
   api("com.typesafe:config:1.4.2")
   api("javax.servlet:javax.servlet-api:4.0.1")
   api("com.google.inject:guice:5.1.0")
@@ -14,9 +14,9 @@ dependencies {
   implementation("org.slf4j:slf4j-api:1.7.36")
   implementation("com.google.inject.extensions:guice-servlet:5.1.0")
   implementation("com.google.guava:guava:31.1-jre")
-  implementation("org.eclipse.jetty:jetty-servlet:9.4.48.v20220622")
-  implementation("org.eclipse.jetty:jetty-server:9.4.48.v20220622")
-  implementation("org.eclipse.jetty:jetty-servlets:9.4.48.v20220622")
+  implementation("org.eclipse.jetty:jetty-servlet:9.4.50.v20221201")
+  implementation("org.eclipse.jetty:jetty-server:9.4.50.v20221201")
+  implementation("org.eclipse.jetty:jetty-servlets:9.4.50.v20221201")
 
   annotationProcessor("org.projectlombok:lombok:1.18.24")
   compileOnly("org.projectlombok:lombok:1.18.24")
--- platform-metrics/build.gradle.kts
+++ platform-metrics/build.gradle.kts
@@ -19,13 +19,13 @@ dependencies {
 
   implementation("io.github.mweirauch:micrometer-jvm-extras:0.2.2")
   implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
+  implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
   implementation("io.dropwizard.metrics:metrics-jvm:4.2.13")
   implementation("io.prometheus:simpleclient_dropwizard:0.12.0")
   implementation("io.prometheus:simpleclient_servlet:0.12.0")
   implementation("io.prometheus:simpleclient_pushgateway:0.12.0")
-  implementation("org.eclipse.jetty:jetty-servlet:9.4.48.v20220622")
-  implementation("com.google.guava:guava:30.1.1-jre")
+  implementation("org.eclipse.jetty:jetty-servlet:9.4.50.v20221201")
+  implementation("com.google.guava:guava:31.1-jre")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
   testImplementation("org.mockito:mockito-core:4.8.0")
