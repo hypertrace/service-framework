6ac8ab2c42d030030ba561397b1f5cb539de98db
SrikarMannepalli <srikar.mannepalli@traceable.ai>
2023-08-18 09:35:06 +0530

    upgrade grpc due to vuln

--- platform-grpc-service-framework/build.gradle.kts
+++ platform-grpc-service-framework/build.gradle.kts
@@ -7,10 +7,10 @@ plugins {
 
 dependencies {
   api(project(":platform-service-framework"))
-  api(platform("io.grpc:grpc-bom:1.56.0"))
+  api(platform("io.grpc:grpc-bom:1.57.2"))
   api("io.grpc:grpc-api")
   api("io.grpc:grpc-services")
-  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.1")
+  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.2")
   api("com.typesafe:config:1.4.2")
   api(project(":service-framework-spi"))
 
@@ -19,5 +19,5 @@ dependencies {
 
   implementation(project(":platform-metrics"))
   implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.12.1")
+  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.12.2")
 }
