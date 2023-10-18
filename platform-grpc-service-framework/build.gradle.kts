aed564ebf0ae6dbfca90cab25fb2b1721f6943cb
Aaron Steinfeld <aaron@traceable.ai>
2023-10-18 14:39:48 -0400

    chore: update framework libs

--- platform-grpc-service-framework/build.gradle.kts
+++ platform-grpc-service-framework/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
   api(platform("io.grpc:grpc-bom:1.57.2"))
   api("io.grpc:grpc-api")
   api("io.grpc:grpc-services")
-  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.2")
+  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.6")
   api("com.typesafe:config:1.4.2")
   api(project(":service-framework-spi"))
 
@@ -19,5 +19,5 @@ dependencies {
 
   implementation(project(":platform-metrics"))
   implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.12.2")
+  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.12.6")
 }
