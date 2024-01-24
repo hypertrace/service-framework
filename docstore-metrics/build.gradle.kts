097f77acd54431ba92fc8db30031478a37793dcb
Suresh Prakash <suresh.prakash@traceable.ai>
2024-01-24 16:00:37 +0530

    Revert unwanted change

--- docstore-metrics/build.gradle.kts
+++ docstore-metrics/build.gradle.kts
@@ -11,5 +11,6 @@ dependencies {
 
   api(libs.hypertrace.documentStore)
   api(project(":service-framework-spi"))
+  api(platform("com.fasterxml.jackson:jackson-bom:2.16.0"))
   implementation(project(":platform-metrics"))
 }
