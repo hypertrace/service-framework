f5e9e0e3edd81d086925fd55e318d399f8747ba1
Aaron Steinfeld <aaron@traceable.ai>
2024-11-13 17:54:59 -0500

    chore: update grpc, netty

--- platform-grpc-service-framework/build.gradle.kts
+++ platform-grpc-service-framework/build.gradle.kts
@@ -7,10 +7,10 @@ plugins {
 
 dependencies {
   api(project(":platform-service-framework"))
-  api(platform("io.grpc:grpc-bom:1.60.0"))
+  api(platform("io.grpc:grpc-bom:1.68.1"))
   api("io.grpc:grpc-api")
   api("io.grpc:grpc-services")
-  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.6")
+  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.7")
   api("com.typesafe:config:1.4.2")
   api(project(":service-framework-spi"))
 
@@ -21,5 +21,5 @@ dependencies {
   implementation("io.grpc:grpc-inprocess")
   implementation("io.grpc:grpc-netty")
   implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.13.6")
+  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.13.7")
 }
