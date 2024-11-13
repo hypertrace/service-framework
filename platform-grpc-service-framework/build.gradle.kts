cc1001d9251fc377c2dcfaffb0cdb0c780232630
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2024-11-13 18:48:54 -0500

    chore: update grpc, netty (#107)
    * chore: update grpc, netty
    
    * chore: add explicit protobuf dep

--- platform-grpc-service-framework/build.gradle.kts
+++ platform-grpc-service-framework/build.gradle.kts
@@ -7,11 +7,12 @@ plugins {
 
 dependencies {
   api(project(":platform-service-framework"))
-  api(platform("io.grpc:grpc-bom:1.60.0"))
+  api(platform("io.grpc:grpc-bom:1.68.1"))
   api("io.grpc:grpc-api")
   api("io.grpc:grpc-services")
-  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.6")
+  api("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.7")
   api("com.typesafe:config:1.4.2")
+  api("com.google.protobuf:protobuf-java:3.25.5")
   api(project(":service-framework-spi"))
 
   annotationProcessor("org.projectlombok:lombok:1.18.24")
@@ -21,5 +22,5 @@ dependencies {
   implementation("io.grpc:grpc-inprocess")
   implementation("io.grpc:grpc-netty")
   implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.13.6")
+  implementation("org.hypertrace.core.grpcutils:grpc-server-utils:0.13.7")
 }
