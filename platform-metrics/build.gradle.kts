c9c5db9afd4dee9faa9d225d492cc5d3ce234b24
Harshit Kumar <harshit.kumar@traceable.ai>
2025-03-06 12:08:32 +0530

    version change

--- platform-metrics/build.gradle.kts
+++ platform-metrics/build.gradle.kts
@@ -12,12 +12,12 @@ tasks.test {
 dependencies {
   api("com.typesafe:config:1.4.2")
   api("io.dropwizard.metrics:metrics-jakarta-servlet:4.2.25")
-  api("io.micrometer:micrometer-core:1.13.6")
+  api("io.micrometer:micrometer-core:1.14.4")
   api("jakarta.servlet:jakarta.servlet-api:6.0.0")
 
   // Using simpleclient flavour since with version >= 1.13.0 micrometer does not support io.prometheus.simpleclient dependencies
   // https://github.com/micrometer-metrics/micrometer/wiki/1.13-Migration-Guide
-  implementation("io.micrometer:micrometer-registry-prometheus-simpleclient:1.13.6")
+  implementation("io.micrometer:micrometer-registry-prometheus-simpleclient:1.14.4")
 
   implementation("io.github.mweirauch:micrometer-jvm-extras:0.2.2")
   implementation("org.slf4j:slf4j-api:1.7.36")
