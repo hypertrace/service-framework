fc0c86e05509c528121ea8a5d667c5a67fe451f0
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2025-06-26 18:28:59 -0400

    ci: update repos (#119)
    * ci: update repos
    
    * ci: force rerun

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.hypertrace.gradle.publishing.HypertracePublishExtension
 import org.hypertrace.gradle.publishing.License
 
 plugins {
-  id("org.hypertrace.repository-plugin") version "0.4.0"
-  id("org.hypertrace.ci-utils-plugin") version "0.3.2"
-  id("org.hypertrace.publish-plugin") version "1.0.2" apply false
-  id("org.hypertrace.jacoco-report-plugin") version "0.2.1" apply false
-  id("org.hypertrace.code-style-plugin") version "1.1.2" apply false
+  id("org.hypertrace.repository-plugin") version "0.5.0"
+  id("org.hypertrace.ci-utils-plugin") version "0.4.0"
+  id("org.hypertrace.publish-plugin") version "1.1.1" apply false
+  id("org.hypertrace.jacoco-report-plugin") version "0.3.0" apply false
+  id("org.hypertrace.code-style-plugin") version "2.1.2" apply false
   id("org.owasp.dependencycheck") version "12.1.0"
 }
 
