6028d4734f3026930a4ed7574a69d8c5704781bb
Varkeychan Jacob <varkey98@gmail.com>
2026-08-03 16:14:14 +0530

    fix vulns

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 configure<DependencyPluginSettingExtension> {
-  catalogVersion.set("0.3.79")
+  catalogVersion.set("0.3.81")
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
